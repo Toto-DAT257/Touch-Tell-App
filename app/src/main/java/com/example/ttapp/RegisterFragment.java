1b61a5d6d043afc06074fc311a1d7497ea64210c
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-04-06 18:41:08 +0200

    Testing

--- app/src/main/java/com/example/ttapp/RegisterFragment.java
+++ app/src/main/java/com/example/ttapp/RegisterFragment.java
@@ -21,8 +21,7 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment Emma är här och härjar
-        // Inflate the layout for this emelie is boss
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_register, container, false);
 
         // Get views
