d3d1fa605e836d2ebf8054baf1fb1557cbaab909
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-04-06 09:49:48 +0200

    Update FirstQuestionFragment.java
    Started creating the first question fragment
    
    Co-Authored-By: amandacyren <71067777+amandacyren@users.noreply.github.com>

--- app/src/main/java/com/example/ttapp/FirstQuestionFragment.java
+++ app/src/main/java/com/example/ttapp/FirstQuestionFragment.java
@@ -1,4 +1,27 @@
 package com.example.ttapp;
 
-public class FirstQuestionFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public abstract class FirstQuestionFragment extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        setView(inflater, container);
+
+        return view;
+
+    }
+
+    protected abstract void setView(LayoutInflater inflater, ViewGroup container);
+
 }
