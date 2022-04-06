c9a90b6fa46b7dad1450a7cad73e3268e8eebc6b
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-04-06 09:52:46 +0200

    refactoring
    Co-Authored-By: amandacyren <71067777+amandacyren@users.noreply.github.com>

--- app/src/main/java/com/example/ttapp/fragments/FirstQuestionFragment.java
+++ app/src/main/java/com/example/ttapp/fragments/FirstQuestionFragment.java
@@ -1,4 +1,4 @@
-package com.example.ttapp;
+package com.example.ttapp.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
---
+++ app/src/main/java/com/example/ttapp/viewmodel/SurveyViewModel.java
@@ -0,0 +1,5 @@
+package com.example.ttapp.viewmodel;
+
+public class SurveyViewModel {
+
+}
