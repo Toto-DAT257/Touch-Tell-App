d9cedf533bf27418cfe3d5380864f38c46d599a0
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-04-06 18:06:48 +0200

    Removed unused imports

--- app/src/main/java/com/example/ttapp/fragments/FirstQuestionFragment.java
+++ app/src/main/java/com/example/ttapp/fragments/FirstQuestionFragment.java
@@ -1,15 +1,10 @@
 package com.example.ttapp.fragments;
 
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.ttapp.R;
 import com.example.ttapp.viewmodel.SurveyViewModel;
