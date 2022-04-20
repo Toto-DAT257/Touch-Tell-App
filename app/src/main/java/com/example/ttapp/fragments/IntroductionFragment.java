22fb2c11dc00d779d3b5094900ae56670f76d26b
Simon Holst <simonhol@student.chalmers.se>
2022-04-20 08:28:13 +0200

    Initiated IntroductionFragment

---
+++ app/src/main/java/com/example/ttapp/fragments/IntroductionFragment.java
@@ -0,0 +1,22 @@
+package com.example.ttapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ttapp.R;
+
+
+public class IntroductionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_introduction, container, false);
+    }
+}
