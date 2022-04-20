53ea219d5ab778180c2ca5ade25585ec6f51bcde
Johan Selin <johanselin99@gmail.com>
2022-04-20 20:12:00 +0200

    ViewModel now cleaned up and working as expected.

--- app/src/main/java/com/example/ttapp/fragments/RegisterFragment.java
+++ app/src/main/java/com/example/ttapp/fragments/RegisterFragment.java
@@ -9,24 +9,18 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ttapp.R;
 import com.example.ttapp.database.MongoDB;
 import com.example.ttapp.databinding.FragmentRegisterBinding;
 import com.example.ttapp.viewmodel.RegisterViewModel;
-
-import org.bson.Document;
-
-import io.realm.mongodb.App;
-import io.realm.mongodb.RealmResultTask;
-import io.realm.mongodb.User;
 
 /**
  * Class for a fragment that presents the sign in screen
@@ -35,7 +29,6 @@ import io.realm.mongodb.User;
  */
 public class RegisterFragment extends Fragment {
 
-    private FragmentRegisterBinding binding;
     private RegisterViewModel registerViewModel;
     private String identifier;
     EditText codeEditText;
@@ -45,9 +38,9 @@ public class RegisterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        binding = FragmentRegisterBinding.inflate(getLayoutInflater());
+        FragmentRegisterBinding binding = FragmentRegisterBinding.inflate(getLayoutInflater());
         View root = binding.getRoot();
+
         registerViewModel = new ViewModelProvider(requireActivity()).get(RegisterViewModel.class);
         registerViewModel.setDatabase(MongoDB.getDatabase(getContext()));
 
@@ -56,24 +49,32 @@ public class RegisterFragment extends Fragment {
         errorCodeIsEmpty = binding.errorCodeIsEmpty;
         errorIdentifierNotFound = binding.errorIdentifierNotFound;
 
-        confirmButton.setOnClickListener(view1 -> {
-            identify();
-            //login(root, codeEditText, errorCodeIsEmpty, errorIdentifierNotFound);
-        });
-        registerViewModel.getIdentified().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean identification) {
-                if (identification) {
-                    saveIdentifier(identifier);
-                    Navigation.findNavController(root).navigate(R.id.action_registerFragment_to_firstQuestionFragment);
-                }
-                else {
-                    errorIdentifierNotFound.setVisibility(View.VISIBLE);
-                }
-            }
-        });
+        confirmButton.setOnClickListener(view1 -> identify());
+
+        observeIdentification(root);
+        observeDatabaseAccess();
         return root;
     }
+
+    private void observeIdentification(View root) {
+        registerViewModel.getIdentified().observe(getViewLifecycleOwner(), identification -> {
+            if (identification) {
+                saveIdentifier(identifier);
+                Navigation.findNavController(root).navigate(R.id.action_registerFragment_to_firstQuestionFragment);
+            } else {
+                errorIdentifierNotFound.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void observeDatabaseAccess() {
+        registerViewModel.getDatabaseAccess().observe(getViewLifecycleOwner(), databaseAccess -> {
+            if (!databaseAccess) {
+                Toast.makeText(getContext(), R.string.SessionExpiredToast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 
     private void identify() {
         identifier = codeEditText.getText().toString();
@@ -82,35 +83,6 @@ public class RegisterFragment extends Fragment {
         } else {
             registerViewModel.identify(identifier);
             errorCodeIsEmpty.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    private void login(View root, EditText codeEditText, TextView errorCodeIsEmpty, TextView errorIdentifierNotFound) {
-        String identifier = codeEditText.getText().toString();
-
-        if (identifier.isEmpty()) {
-            errorCodeIsEmpty.setVisibility(View.VISIBLE);
-        } else {
-            errorCodeIsEmpty.setVisibility(View.INVISIBLE);
-            MongoDB db = MongoDB.getDatabase(getContext());
-            RealmResultTask<Document> task = db.getDeviceIdTask(identifier);
-            task.getAsync(result -> {
-                if (result.isSuccess()) {
-                    if (result.get() != null) {
-                        saveIdentifier(identifier);
-                        Navigation.findNavController(root).navigate(R.id.action_registerFragment_to_firstQuestionFragment);
-                        Log.v("LOGIN", "Identifier " + result.get().toString());
-                    }
-                    else {
-                        Log.v("LOGIN", "Identifier not registered");
-                        errorIdentifierNotFound.setVisibility(View.VISIBLE);
-                    }
-                }
-                else {
-                    Log.v("LOGIN", "Database access failed." + result.getError().toString());
-                    MongoDB.getMongoApp().currentUser().logOutAsync(result1 -> Log.v("LOGOUT:", String.valueOf(result1.isSuccess())));
-                }
-            });
         }
     }
 
