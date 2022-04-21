1179524b4d631c98bc393d520f14af04cf1ac638
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-04-21 12:10:04 +0200

    Created surveyfragment
    Co-Authored-By: amandacyren <71067777+amandacyren@users.noreply.github.com>
    Co-Authored-By: Simon Holst <69155090+simonholst@users.noreply.github.com>

---
+++ app/src/main/java/com/example/ttapp/fragments/SurveyFragment.java
@@ -0,0 +1,32 @@
+package com.example.ttapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.ttapp.R;
+import com.example.ttapp.viewmodel.SurveyViewModel;
+
+
+public class SurveyFragment extends Fragment {
+
+    SurveyViewModel surveyViewModel;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_survey, container, false);
+        surveyViewModel = new ViewModelProvider(requireActivity()).get(SurveyViewModel.class);
+        surveyViewModel.loadQuestions(getContext(), getActivity());
+
+        return view;
+    }
+
+}
