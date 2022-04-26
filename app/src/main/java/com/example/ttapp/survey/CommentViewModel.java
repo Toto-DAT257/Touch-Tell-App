c6735c1bd3ad05e28579e4193113d4b7a3eb6e7e
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-04-26 10:02:36 +0200

    Implemented methods in CommentFragment according to abstract QuestionFragment

--- app/src/main/java/com/example/ttapp/survey/CommentViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.ttapp.survey;
-
-import androidx.lifecycle.ViewModel;
-
-public class CommentViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
---
+++ app/src/main/java/com/example/ttapp/survey/fragments/CommentFragment.java
@@ -0,0 +1,46 @@
+package com.example.ttapp.survey.fragments;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ttapp.R;
+import com.example.ttapp.survey.viewmodel.CommentViewModel;
+
+/**
+ * Class for a fragment that presents a comment-question
+ *
+ * Used by: -
+ * Uses: -
+ *
+ * Created by
+ * @author Emma Stålberg
+ */
+public class CommentFragment extends QuestionFragment {
+
+    private CommentViewModel commentViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_comment, container, false);
+    }
+
+    @Override
+    protected void setView(LayoutInflater inflater, ViewGroup container) {
+        view = inflater.inflate(R.layout.fragment_comment, container, false);
+    }
+
+    @Override
+    protected void setViewModel() {
+        commentViewModel = new ViewModelProvider(requireActivity()).get(CommentViewModel.class);
+    }
+
+}
