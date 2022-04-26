1af3cc848c905b4433ea327f3042f06fd3cdcfcd
Simon Holst <simonhol@student.chalmers.se>
2022-04-26 09:42:01 +0200

    Added elements to the SurveyFragment and connected them through the binder

--- app/src/main/java/com/example/ttapp/fragments/SurveyFragment.java
+++ app/src/main/java/com/example/ttapp/fragments/SurveyFragment.java
@@ -3,14 +3,17 @@ package com.example.ttapp.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.ttapp.R;
+import com.example.ttapp.databinding.FragmentSurveyBinding;
 import com.example.ttapp.viewmodel.SurveyViewModel;
 
 /**
@@ -20,17 +23,36 @@ import com.example.ttapp.viewmodel.SurveyViewModel;
  */
 public class SurveyFragment extends Fragment {
 
+    FragmentSurveyBinding binding;
     SurveyViewModel surveyViewModel;
+    Button backButton;
+    Button nextButton;
+    FragmentContainerView questionFragmentContainer;
+    TextView questionTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_survey, container, false);
+
+        binding = FragmentSurveyBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+
         surveyViewModel = new ViewModelProvider(requireActivity()).get(SurveyViewModel.class);
+        backButton = binding.surveyBackButton;
+        nextButton = binding.surveyNextButton;
+        questionFragmentContainer = binding.questionFragmentContainer;
+        questionTextView = binding.questionTextView;
+
+        // TODO: add observer listening after change in the question text 
+
+
+
+
+        // TODO: Remove at some point
         surveyViewModel.loadQuestions(getContext(), getActivity());
 
-        return view;
+
+        return root;
     }
 
 }
