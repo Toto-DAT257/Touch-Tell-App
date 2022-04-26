d31ddc61130ffe2a53ba3c79018a115929e08beb
Johan Selin <johanselin99@gmail.com>
2022-04-26 10:32:14 +0200

    Question types changed from enum to static strings

--- app/src/main/java/com/example/ttapp/survey/model/QuestionType.java
+++ app/src/main/java/com/example/ttapp/survey/model/QuestionType.java
@@ -1,17 +1,8 @@
 package com.example.ttapp.survey.model;
 
-public enum QuestionType {
+public final class QuestionType {
+    public static final String YES_NO = "yes-no";
+    public static final String ONE_TO_TEN = "nps";
 
-    YES_NO("yes-no"),
-    ONE_TO_TEN("nps");
-
-    String type;
-
-    QuestionType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
+    private QuestionType() {}
 }
