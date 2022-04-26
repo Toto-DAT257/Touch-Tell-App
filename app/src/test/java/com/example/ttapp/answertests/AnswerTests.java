8e26ac35a42c2fce3b8ad7a1150745f83e433cda
Johan Selin <johanselin99@gmail.com>
2022-04-26 11:57:19 +0200

    NPS answer now done and tested

--- app/src/test/java/com/example/ttapp/answertests/AnswerTests.java
+++ app/src/test/java/com/example/ttapp/answertests/AnswerTests.java
@@ -1,11 +1,13 @@
 package com.example.ttapp.answertests;
 
+import com.example.ttapp.survey.model.answers.NPSAnswer;
 import com.example.ttapp.survey.model.answers.YesNoAnswer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class AnswerTests {
@@ -23,9 +25,20 @@ public class AnswerTests {
 
         YesNoAnswer answera = mapper.readValue(jsona, YesNoAnswer.class);
         YesNoAnswer answerb = mapper.readValue(jsonb, YesNoAnswer.class);
-        assertThat(answera.answerIsYes).isTrue();
+        assertThat(answera.answerValue).isEqualTo(YesNoAnswer.YES);
         assertThat(answera.questionId).isEqualTo(questionId);
-        assertThat(answerb.answerIsYes).isFalse();
+        assertThat(answerb.answerValue).isEqualTo(YesNoAnswer.NO);
         assertThat(answerb.questionId).isEqualTo(questionId);
+    }
+
+    @Test
+    public void recreateNPSObjectFromJson() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        NPSAnswer a = new NPSAnswer(1, "npsquestionid");
+        String jsona = a.getAnswerJson();
+        NPSAnswer answerObject = mapper.readValue(jsona, NPSAnswer.class);
+        assertThat(answerObject.value).isEqualTo(a.value);
+        assertThat(answerObject.questionId).isEqualTo(a.questionId);
     }
 }
