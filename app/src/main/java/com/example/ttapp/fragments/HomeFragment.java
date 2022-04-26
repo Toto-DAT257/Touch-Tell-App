9d6dfc4a85688736ff1ec28ef3b528e09eb9f871
amandacyren <71067777+amandacyren@users.noreply.github.com>
2022-04-26 09:36:00 +0200

    Added header and buttons to fragment and strings
    Co-Authored-By: Philip Winsnes <phiwin@student.chalmers.se>

--- app/src/main/java/com/example/ttapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/ttapp/fragments/HomeFragment.java
@@ -2,7 +2,9 @@ package com.example.ttapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
@@ -11,6 +13,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.ttapp.R;
+import com.example.ttapp.database.MongoDB;
+import com.example.ttapp.databinding.FragmentHomeBinding;
+import com.example.ttapp.viewmodel.RegisterViewModel;
 
 /**
  * Class for a fragment that presents the home page to the application
@@ -23,13 +28,20 @@ import com.example.ttapp.R;
  */
 public class HomeFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-    Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    View view = inflater.inflate(R.layout.fragment_home, container, false);
+    private Button buttonStartSurvey;
 
-    return view;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FragmentHomeBinding binding = FragmentHomeBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+
+        buttonStartSurvey = binding.buttonStartSurvey;
+
+        buttonStartSurvey.setOnClickListener(view1 -> {
+            Navigation.findNavController(view1).navigate(R.id.action_homeFragment_to_firstQuestionFragment);
+        });
+
+        return root;
     }
 
 }
