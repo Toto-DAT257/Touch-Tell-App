44893c68446c5930dcfea25cfb8617a3712017e8
Emelie Edberg <familjen.emelie@gmail.com>
2022-04-29 10:49:32 +0200

    added javaDoc in JsonQuestionParser

--- app/src/main/java/com/example/ttapp/survey/model/JsonQuestionsParser.java
+++ app/src/main/java/com/example/ttapp/survey/model/JsonQuestionsParser.java
@@ -41,18 +41,37 @@ public class JsonQuestionsParser {
         return ids;
     }
 
+    /**
+     * Gets the questionId of the first question in the survey
+     * @return the id of the first question
+     */
     public String getFirstQuestionId() {
         return questionOrder.get(0);
     }
 
+    /**
+     * Checks if a question is the last question of the survey
+     * @param id id of the question
+     * @return true if the question is last,  false otherwise
+     */
     public boolean isLastQuestion(String id) {
         return questionOrder.get(questionOrder.size() - 1).equals(id);
     }
 
+    /**
+     * Checks if a question is the first question of the survey
+     * @param id id of the question
+     * @return true if the question is first,  false otherwise
+     */
     public boolean isFirstQuestion(String id) {
         return questionOrder.get(0).equals(id);
     }
-
+    /**
+     * Gets the questionId of the question coming next after a question
+     * @param currentId id of the question
+     * @return the id of the next coming question
+     * Trows exception if the question is last in the survey or the question don't exist in the survey
+     */
     public String getNextQuestionId(String currentId) {
         if (isLastQuestion(currentId)) {
             throw new IndexOutOfBoundsException("currentId is the last question, no next question exist");
@@ -66,6 +85,12 @@ public class JsonQuestionsParser {
         throw new IllegalArgumentException("currentId does not exist in the survey");
     }
 
+    /**
+     * Gets the questionId of a the question preceding a question
+     * @param currentId id of the question
+     * @return the id of the previous question
+     * Trows exception if the question is first in the survey or the question don't exist in the survey
+     */
     public String getPreviousQuestionId(String currentId) {
         if (isFirstQuestion(currentId)) {
             throw new IndexOutOfBoundsException("currentId is the firs question, no previous question exist");
@@ -90,6 +115,12 @@ public class JsonQuestionsParser {
         throw new IllegalArgumentException("id does not exist in the survey");
     }
 
+    /**
+     * Gets the question text of a question
+     * @param id id of the question
+     * @return the question text of the question
+     * Trows exception if the question is missing a swedish translation (default language)
+     */
     public String getQuestionText(String id) {
         for (Languages l : getQuestion(id).questionText) {
             if (l.language.equals(SWEDISH)) {
@@ -103,14 +134,30 @@ public class JsonQuestionsParser {
         return survey.questions.get(getQuestionNumber(id));
     }
 
+    /**
+     * Gets the type of a question
+     * @param id id of the question
+     * @return the type of the question
+     */
     public String getType(String id) {
         return survey.questions.get(getQuestionNumber(id)).questionType;
     }
 
+    /**
+     * Checks if a question has any conditions
+     * @param id id of the question
+     * @return true if condition exist, false otherwise
+     */
     public boolean conditionExist(String id) {
         return getQuestion(id).conditions != null;
     }
 
+    /**
+     * returns a list with conditions for a question
+     * @param id id of the question
+     * @return list with conditions
+     * Throws exception if the question dose not have any conditions
+     */
     public List<Condition> getConditions(String id) {
         if (!conditionExist(id)) {
             throw new IllegalArgumentException("This question has no conditions");
