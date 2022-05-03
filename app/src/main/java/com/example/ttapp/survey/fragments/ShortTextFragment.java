fb81965d133f0bb1947a078d6b52df707b6cafe6
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-05-03 11:40:58 +0200

    Deleted irrelevant methods and added needed methods

--- app/src/main/java/com/example/ttapp/survey/fragments/ShortTextFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/ShortTextFragment.java
@@ -1,38 +1,23 @@
 package com.example.ttapp.survey.fragments;
 
-import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.ttapp.R;
+public class ShortTextFragment extends QuestionFragment {
 
-public class ShortTextFragment extends Fragment {
+    @Override
+    protected void setView(LayoutInflater inflater, ViewGroup container) {
 
-    private ShortTextViewModel mViewModel;
-
-    public static ShortTextFragment newInstance() {
-        return new ShortTextFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_short_text, container, false);
+    protected void initResponseOptions() {
+
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ShortTextViewModel.class);
-        // TODO: Use the ViewModel
+    protected void initSaveResponseObserver() {
+
     }
 
 }
