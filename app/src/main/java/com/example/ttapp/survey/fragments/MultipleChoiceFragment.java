ccf889ee8e8f7c1a6a10cccd9b61f849cd5eb936
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-05-03 11:15:42 +0200

    First draft for trying to  dynamically add scrollview

--- app/src/main/java/com/example/ttapp/survey/fragments/MultipleChoiceFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/MultipleChoiceFragment.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import com.example.ttapp.R;
 
@@ -20,6 +22,8 @@ public class MultipleChoiceFragment extends QuestionFragment {
 
     private final ArrayList<Integer> response = new ArrayList<>();
 
+    private LinearLayout linearLayout;
+
     @Override
     protected void setView(LayoutInflater inflater, ViewGroup container) {
         view = inflater.inflate(R.layout.fragment_multiple_choice, container, false);
@@ -27,7 +31,12 @@ public class MultipleChoiceFragment extends QuestionFragment {
 
     @Override
     protected void initResponseOptions() {
-
+        linearLayout = view.findViewById(R.id.multipleChoiceLinearLayout);
+        for (int i = 0; i < 5; i++) {
+            CheckBox ch = new CheckBox(requireActivity());
+            ch.setText(i);
+            linearLayout.addView(ch);
+        }
     }
 
     @Override
