31fec04dd5ee140c18b9bf214e1a571dcbf32d1c
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-05-03 11:53:30 +0200

    Started implementing ShortTextFragment

--- app/src/main/java/com/example/ttapp/survey/fragments/ShortTextFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/ShortTextFragment.java
@@ -2,17 +2,23 @@ package com.example.ttapp.survey.fragments;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.ttapp.R;
 
 public class ShortTextFragment extends QuestionFragment {
 
+    private EditText shortTextResponse;
+    private String response;
+
     @Override
     protected void setView(LayoutInflater inflater, ViewGroup container) {
-
+        view = inflater.inflate(R.layout.fragment_short_text, container, false);
     }
 
     @Override
     protected void initResponseOptions() {
-
+        shortTextResponse = view.findViewById(R.id.shortTextResponse);
     }
 
     @Override
