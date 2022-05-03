8cdfc3486137b00869b73aaacf0697544ce1f4d2
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-05-03 13:04:15 +0200

    Deleted irrelevant code

--- app/src/main/java/com/example/ttapp/survey/fragments/NumberFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/NumberFragment.java
@@ -1,38 +1,22 @@
 package com.example.ttapp.survey.fragments;
 
-import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.ttapp.R;
+public class NumberFragment extends QuestionFragment {
 
-public class NumberFragment extends Fragment {
+    @Override
+    protected void setView(LayoutInflater inflater, ViewGroup container) {
 
-    private NumberViewModel mViewModel;
-
-    public static NumberFragment newInstance() {
-        return new NumberFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_number, container, false);
+    protected void initResponseOptions() {
+
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(NumberViewModel.class);
-        // TODO: Use the ViewModel
-    }
+    protected void initSaveResponseObserver() {
 
+    }
 }
--- app/src/main/java/com/example/ttapp/survey/fragments/NumberViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.ttapp.survey.fragments;
-
-import androidx.lifecycle.ViewModel;
-
-public class NumberViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
