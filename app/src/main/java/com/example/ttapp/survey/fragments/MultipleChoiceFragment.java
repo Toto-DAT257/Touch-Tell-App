2328085954d5678a1f8d23de4507160501f7a359
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-05-03 11:23:51 +0200

    Deleted unneccessary imports

--- app/src/main/java/com/example/ttapp/survey/fragments/MultipleChoiceFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/MultipleChoiceFragment.java
@@ -1,15 +1,6 @@
 package com.example.ttapp.survey.fragments;
 
-import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
