a89d445abadb2107269655ace458f1abde01ffef
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-05-03 11:30:55 +0200

    Created select many fragment

---
+++ app/src/main/java/com/example/ttapp/survey/fragments/SelectManyFragment.java
@@ -0,0 +1,38 @@
+package com.example.ttapp.survey.fragments;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ttapp.R;
+
+public class SelectManyFragment extends Fragment {
+
+    private SelectManyViewModel mViewModel;
+
+    public static SelectManyFragment newInstance() {
+        return new SelectManyFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_select_many, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SelectManyViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/ttapp/survey/fragments/SelectManyViewModel.java
@@ -0,0 +1,7 @@
+package com.example.ttapp.survey.fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class SelectManyViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
