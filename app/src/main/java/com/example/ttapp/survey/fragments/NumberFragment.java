5b1037a3f3517b76816cebec9a7dd7738dfd7e8d
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-05-04 09:48:37 +0200

    NumberFragment now has all relevant logic.

--- app/src/main/java/com/example/ttapp/survey/fragments/NumberFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/NumberFragment.java
@@ -2,21 +2,31 @@ package com.example.ttapp.survey.fragments;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.ttapp.R;
 
 public class NumberFragment extends QuestionFragment {
 
+    private String response;
+    private EditText numberResponse;
+
     @Override
     protected void setView(LayoutInflater inflater, ViewGroup container) {
-
+        view = inflater.inflate(R.layout.fragment_number, container, false);
     }
 
     @Override
     protected void initResponseOptions() {
-
+        numberResponse = view.findViewById(R.id.numberResponse);
     }
 
     @Override
     protected void initSaveResponseObserver() {
-
+        surveyViewModel.getSaveResponse().observe(getViewLifecycleOwner(), bool -> {
+            response = numberResponse.getText().toString();
+            surveyViewModel.saveResponse(response);
+        });
     }
+
 }
