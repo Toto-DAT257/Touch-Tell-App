f2121c0ec2de807bb47a3a3385094ce762ba7ef2
fannysoderling <fannysoderling@gmail.com>
2022-05-04 15:26:50 +0200

    implementation of design is done

--- app/src/main/java/com/example/ttapp/survey/fragments/MultipleChoiceFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/MultipleChoiceFragment.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.ttapp.ListAdapter;
@@ -39,7 +41,7 @@ public class MultipleChoiceFragment extends QuestionFragment {
         adapter = new ListAdapter(requireActivity().getApplicationContext(), content);
         listView.setAdapter(adapter);
 
-
+        initClickOnListItem();
 
     }
 
@@ -47,9 +49,18 @@ public class MultipleChoiceFragment extends QuestionFragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-               // adapterView.getItemAtPosition(i);
-                TextView textView = view.findViewById(R.id.textview_multibutton);
-                textView.setText("Hello World!");
+                // adapterView.getItemAtPosition(i);
+                ImageView multiCheck = view.findViewById(R.id.check_multibutton);
+                ConstraintLayout multiConstraintLayout = view.findViewById(R.id.multiConstraintLayout);
+
+                if (multiCheck.getVisibility() == View.INVISIBLE) {
+                    multiCheck.setVisibility(View.VISIBLE);
+                    multiConstraintLayout.setBackgroundResource(R.drawable.background_multibutton_light);
+                }
+                else {
+                    multiCheck.setVisibility(View.INVISIBLE);
+                    multiConstraintLayout.setBackgroundResource(R.drawable.background_multibutton); 
+                }
             }
         });
     }
