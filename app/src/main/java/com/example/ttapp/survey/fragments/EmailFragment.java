ad82223736fc9f92a09e612aead6d741c0270760
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-05-04 10:13:49 +0200

    Email fragment now has relevant logic, relevant textfields and so on.

--- app/src/main/java/com/example/ttapp/survey/fragments/EmailFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/EmailFragment.java
@@ -1,38 +1,32 @@
 package com.example.ttapp.survey.fragments;
 
-import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.ttapp.R;
 
-public class EmailFragment extends Fragment {
+public class EmailFragment extends QuestionFragment {
 
-    private EmailViewModel mViewModel;
+    private EditText emailResponse;
+    private String response;
 
-    public static EmailFragment newInstance() {
-        return new EmailFragment();
+    @Override
+    protected void setView(LayoutInflater inflater, ViewGroup container) {
+        view = inflater.inflate(R.layout.fragment_email, container, false);
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_email, container, false);
+    protected void initResponseOptions() {
+        emailResponse = view.findViewById(R.id.emailResponse);
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(EmailViewModel.class);
-        // TODO: Use the ViewModel
+    protected void initSaveResponseObserver() {
+        surveyViewModel.getSaveResponse().observe(getViewLifecycleOwner(), bool -> {
+            response = emailResponse.getText().toString();
+            surveyViewModel.saveResponse(response);
+        });
     }
 
 }
