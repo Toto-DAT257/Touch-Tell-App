5ee28691542e4d001c0827fc92731d42ded21607
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2022-05-09 14:00:38 +0200

    Created observer for commentresponse. Sets already answered text into the textview.
    Co-authored-by: amandacyren <71067777+amandacyren@users.noreply.github.com>

--- app/src/main/java/com/example/ttapp/survey/fragments/CommentFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/CommentFragment.java
@@ -4,6 +4,8 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import androidx.lifecycle.Observer;
+
 import com.example.ttapp.R;
 
 /**
@@ -35,6 +37,16 @@ public class CommentFragment extends QuestionFragment {
         surveyViewModel.getSaveResponse().observe(getViewLifecycleOwner(), bool -> {
             response = commentResponse.getText().toString();
             surveyViewModel.saveResponse(response);
+        });
+    }
+
+    @Override
+    protected void initResponseObserver() {
+        surveyViewModel.containsCommentresponse().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                commentResponse.setText(s);
+            }
         });
     }
 
