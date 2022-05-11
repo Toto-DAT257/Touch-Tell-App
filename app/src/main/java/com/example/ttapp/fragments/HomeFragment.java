c92d15bc9c9dd961b2903683824c95805fec66fd
Simon Holst <simonhol@student.chalmers.se>
2022-05-11 10:00:35 +0200

    The status bar now dynamically changes color depending on wether the user section is open in HomeFragment or not
    Co-Authored-By: Philip Winsnes <phiwin@student.chalmers.se>

--- app/src/main/java/com/example/ttapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/ttapp/fragments/HomeFragment.java
@@ -4,10 +4,15 @@ import static android.view.View.GONE;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -53,7 +58,7 @@ public class HomeFragment extends Fragment {
         userContainer = binding.userContainer;
         textViewHomeHeader = binding.textViewHomeHeader;
 
-        buttonStartSurvey.setOnClickListener(view1 -> Navigation.findNavController(root).navigate(R.id.action_homeFragment_to_surveyFragment));
+        buttonStartSurvey.setOnClickListener(view1 -> startSurvey(root));
 
         buttonSignOut.setOnClickListener(view -> signOut(root));
 
@@ -65,47 +70,82 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
+    private void startSurvey(View view) {
+        changeStatusBarColor(R.color.toto_background_gradient_blue);
+        Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_surveyFragment);
+    }
+
     private void signOut(View view) {
+        changeStatusBarColor(R.color.toto_background_gradient_blue);
         forgetIdentifier();
         Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_registerFragment);
     }
 
-    public void closeUserSection(View view) {
-        buttonUser.setEnabled(true);
+    public void closeUserSection() {
+        buttonUser.setEnabled(false);
+        buttonCloseUserSection.setEnabled(false);
         TranslateAnimation animate = new TranslateAnimation(
-                0,                 // fromXDelta
-                0,                 // toXDelta
-                0,  // fromYDelta
-                -view.getHeight());                // toYDelta
+                0,
+                0,
+                0,
+                -userContainer.getHeight());
         animate.setDuration(500);
         animate.setFillAfter(true);
-        view.startAnimation(animate);
-        view.setVisibility(GONE);
+        userContainer.startAnimation(animate);
+        userContainer.setVisibility(GONE);
+        new CountDownTimer(500,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) { }
+
+            @Override
+            public void onFinish() {
+                buttonUser.setEnabled(true);
+                buttonCloseUserSection.setEnabled(true);
+                changeStatusBarColor(R.color.toto_background_gradient_blue);
+            }
+        }.start();
     }
 
-    public void openUserSection(View view) {
-        view.setVisibility(View.VISIBLE);
+    public void openUserSection() {
+        buttonUser.setEnabled(false);
+        buttonCloseUserSection.setEnabled(false);
+        changeStatusBarColor(R.color.toto_dark_grey);
+        userContainer.setVisibility(View.VISIBLE);
         TranslateAnimation animate = new TranslateAnimation(
-                0,                 // fromXDelta
-                0,                 // toXDelta
-                -view.getHeight(),                 // fromYDelta
-                0); // toYDelta
+                0,
+                0,
+                -userContainer.getHeight(),
+                0);
         animate.setDuration(500);
         animate.setFillAfter(true);
-        view.startAnimation(animate);
-        buttonUser.setEnabled(false);
+        userContainer.startAnimation(animate);
+        new CountDownTimer(500,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) { }
+
+            @Override
+            public void onFinish() {
+                buttonCloseUserSection.setEnabled(true);
+                buttonCloseUserSection.setEnabled(true);
+            }
+        }.start();
     }
 
     public void onUserSectionButtonClick() {
         if (userSectionIsOpen) {
-            closeUserSection(userContainer);
+            closeUserSection();
         } else {
-            openUserSection(userContainer);
+            openUserSection();
         }
         userSectionIsOpen = !userSectionIsOpen;
     }
 
-
+    private void changeStatusBarColor(int color) {
+        Window window = getActivity().getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(getResources().getColor(color));
+    }
 
     private void forgetIdentifier() {
         sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE);
