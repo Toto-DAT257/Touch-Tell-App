d1b4d4c33be8aa2605a4e37be5eeb689c781d0bd
alvajansson <alvajjansson@hotmail.com>
2022-05-12 08:30:06 +0200

    Upload branch TA-89 so Fanny can pull and work with the same code

--- app/src/main/java/com/example/ttapp/survey/fragments/SmileyCommentFragment.java
+++ app/src/main/java/com/example/ttapp/survey/fragments/SmileyCommentFragment.java
@@ -1,10 +1,13 @@
 package com.example.ttapp.survey.fragments;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 
 import com.example.ttapp.R;
 
@@ -25,9 +28,11 @@ public class SmileyCommentFragment extends QuestionFragment {
     private ImageView smileyCResponseoption2;
     private ImageView smileyCResponseoption3;
     private ImageView smileyCResponseoption4;
+    private TableLayout tableSmileys;
 
     private EditText smileyCComment;
-    Button buttonCommentSmiley;
+    private Button buttonCommentSmiley;
+    private Button buttonBackSmiley;
 
     ArrayList<Integer> responseOption = new ArrayList<>();
     String comment;
@@ -40,14 +45,18 @@ public class SmileyCommentFragment extends QuestionFragment {
     @Override
     protected void initResponseOptions() {
         smileyCComment = view.findViewById(R.id.smileyCComment);
+        smileyCComment.setVisibility(View.INVISIBLE);
 
+        tableSmileys = view.findViewById(R.id.tableSmileys);
         smileyCResponseoption1 = view.findViewById(R.id.smileyCResponseoption1);
         smileyCResponseoption2 = view.findViewById(R.id.smileyCResponseoption2);
         smileyCResponseoption3 = view.findViewById(R.id.smileyCResponseoption3);
         smileyCResponseoption4 = view.findViewById(R.id.smileyCResponseoption4);
 
         buttonCommentSmiley = view.findViewById(R.id.buttonCommentSmiley);
-
+        buttonCommentSmiley.setVisibility(View.INVISIBLE);
+        buttonBackSmiley = view.findViewById(R.id.buttonBackSmiley);
+        buttonBackSmiley.setVisibility(View.INVISIBLE);
         initOnClickListeners();
     }
 
@@ -56,11 +65,45 @@ public class SmileyCommentFragment extends QuestionFragment {
         surveyViewModel.getSaveResponse().observe(getViewLifecycleOwner(), bool -> {
             comment = smileyCComment.getText().toString();
             // TODO set responseOption when design is done
-            surveyViewModel.saveResponse(responseOption, comment);
+
+            surveyViewModel.saveResponse(responseOption, comment); //responseOption.get(responseOption.size()-1)
         });
     }
 
     private void initOnClickListeners() {
+        smileyCResponseoption1.setOnClickListener(view -> {
+            responseOption.add(1);
+            buttonCommentSmiley.setVisibility(View.VISIBLE);
+        });
+
+        smileyCResponseoption2.setOnClickListener(view -> {
+            responseOption.add(2);
+            buttonCommentSmiley.setVisibility(View.VISIBLE);
+        });
+
+        smileyCResponseoption3.setOnClickListener(view -> {
+            responseOption.add(3);
+            buttonCommentSmiley.setVisibility(View.VISIBLE);
+        });
+
+        smileyCResponseoption4.setOnClickListener(view -> {
+            responseOption.add(4);
+            buttonCommentSmiley.setVisibility(View.VISIBLE);
+        });
+
+        buttonCommentSmiley.setOnClickListener(view ->{
+            buttonCommentSmiley.setVisibility(View.INVISIBLE);
+            buttonBackSmiley.setVisibility(View.VISIBLE);
+            tableSmileys.setVisibility(View.INVISIBLE);
+            smileyCComment.setVisibility(View.VISIBLE);
+        });
+
+        buttonBackSmiley.setOnClickListener(view -> {
+            buttonCommentSmiley.setVisibility(View.VISIBLE);
+            buttonBackSmiley.setVisibility(View.INVISIBLE);
+            tableSmileys.setVisibility(View.VISIBLE);
+            smileyCComment.setVisibility(View.INVISIBLE);
+        });
 
     }
 
